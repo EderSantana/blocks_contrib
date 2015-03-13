f3c6632eae93521025f341e55dcbef7084b3ebf6
edersantana <edersantanajunior@hotmail.com>
2015-03-12 21:17:17 -0400

    added pylearn2dataset with noise

--- pylearn2fuel/__init__.py
+++ pylearn2fuel/__init__.py
@@ -1,4 +1,5 @@
 from fuel.datasets import Dataset
+import numpy as np
 
 class Pylearn2Dataset(Dataset):
     '''Pylearn2Dataset wraps a `pylearn2.dataset` object and adds only the
@@ -28,3 +29,36 @@ class Pylearn2Dataset(Dataset):
         return iterator    
     def get_data(self,state=None,request=None):
         return next(state)
+
+class Pylearn2DatasetNoise(Dataset):
+    '''Pylearn2DatasetNoise is the same as `Pylearn2Dataset` with some an 
+    extra batch of random nubmer.
+    
+    Parameters
+    ----------
+    dataset: `pylearn2.dataset` object 
+        Note that this is expecting the actual the object will be initialized inside
+    batch_size: int
+        Batch size to be used by the `pylearn2.dataset` iterator.
+    noise_dim: int
+        Dimmension of the noise batch
+    '''
+    def __init__(self, dataset, batch_size, noise_dim,  **kwargs):
+        self.pylearn2_dataset = dataset
+        self.sources = self.pylearn2_dataset.get_data_specs()[1]
+        self.batch_size = batch_size 
+        self.noise_dim = noise_dim
+    def open(self):
+        num_examples = self.pylearn2_dataset.get_num_examples()
+        iterator = self.pylearn2_dataset.iterator(
+                   self.batch_size,
+                   num_examples/self.batch_size,
+                   mode='sequential',
+                   data_specs=self.pylearn2_dataset.get_data_specs(),
+                   return_tuple=True)
+        return iterator    
+    def get_data(self,state=None,request=None):
+        batch = next(state)
+        eps = np.random.normal(0,1,size=self.noise_dim)
+        batch.append(eps)
+        return batch
