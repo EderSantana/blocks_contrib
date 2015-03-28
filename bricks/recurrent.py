85cd92f5e77c6200431713f389ee827b1b72bcaf
EderSantana <eder@macbook.com>
2015-03-27 22:49:28 -0400

    condrec

--- bricks/recurrent.py
+++ bricks/recurrent.py
@@ -7,90 +7,29 @@ from blocks.roles import add_role, WEIGHT
 from blocks.utils import shared_floatx_nans
 from blocks.bricks.recurrent import BaseRecurrent, recurrent
 
-class UnfolderLSTMTake2(Initializable, BaseRecurrent):
-    """UnfolderLSTM network
 
-    A recurrent network that unfolds an input vector to a sequence.
+class ConditionedRecurrent(BaseRecurrent):
 
-    Parameters
-    ----------
-    prototype : instance of :class:`LSTM`
-        A brick that will get the input vector.
-
-    flagger : instance of :class:`Brick``
-        A brick that will flag when to stop the loop
-
-    Notes
-    -----
-    See :class:`.Initializable` for initialization parameters.
-
-    """
-    @lazy
-    def __init__(self, prototype, **kwargs):
-        super(UnfolderLSTMTake2, self).__init__(**kwargs)
-        self.children = [prototype]
+    def __init__(self, wrapped, **kwargs):
+        super(ConditionedRecurrent, self).__init__(**kwargs)
+        self.wrapped = wrapped
+        self.children=[wrapped]
 
     def get_dim(self, name):
-        if name in ('inputs', 'states', 'cells'):
-            return self.children[0].get_dim(name)
-        else:
-            return super(UnfolderLSTMTake2, self).get_dim(name)
+        if name == 'context':
+            return self.wrapped.get_dim('inputs')
+        if name == 'context_mask':
+            return 0
+        return self.wrapped.get_dim(name)
 
-    #def initial_state(self, state_name, batch_size, *args, **kwargs):
+    @application(contexts=['context', 'context_mask'])
+    def apply(self, **kwargs):
+        context = kwargs['context']
+        kwargs['inputs'] += context.dimshuffle('x',0,1)
+        for context in ConditionedRecurrent.apply.contexts:
+            kwargs.pop(context)
+        return self.wrapped.apply(**kwargs)
 
-    @recurrent(sequences=[], states=['states', 'cells'],
-               outputs=['states','cells'], contexts=['inputs'])
-    def apply(self, inputs=None, states=None, cells=None, **kwargs):
-        outputs = self.children[0].apply(inputs=inputs,
-                                         cells=cells,
-                                         states=states,
-                                         iterate=False,
-                                         **kwargs)
-        return outputs
-
-class UnfolderTake2(Initializable, BaseRecurrent):
-    """Unfolder network
-
-    A recurrent network that unfolds an input vector to a sequence.
-
-    Parameters
-    ----------
-    prototype : instance of :class:`BaseRecurrent`
-        A brick that will get the input vector.
-
-    flagger : instance of :class:`Brick``
-        A brick that will flag when to stop the loop
-
-    Notes
-    -----
-    See :class:`.Initializable` for initialization parameters.
-
-    """
-    @lazy
-    def __init__(self, prototype, flagger, **kwargs):
-        super(Unfolder, self).__init__(**kwargs)
-        self.children = [prototype, flagger]
-
-    def get_dim(self, name):
-        if name in ('inputs', 'states'):
-            return self.children[0].dim
-        else:
-            return super(Unfolder, self).get_dim(name)
-
-    #def initial_state(self, state_name, batch_size, *args, **kwargs):
-
-    @recurrent(sequences=[], states=['states'], outputs=['states', 'flags'],
-               contexts=['inputs'])
-    def apply(self, inputs=None, states=None, **kwargs):
-        outputs = self.children[0].apply(
-                           inputs=inputs,
-                           states=states,
-                           iterate=False,
-                           **kwargs)
-        flags = self.children[1].apply(outputs).sum()
-        outputs = [outputs, flags]
-        return outputs
-    #TODO define outputs_info, define RecurrentFlag class
 
 class Unfolder(Initializable, BaseRecurrent):
     """Unfolder network
@@ -123,7 +62,8 @@ class Unfolder(Initializable, BaseRecurrent):
 
     #def initial_state(self, state_name, batch_size, *args, **kwargs):
 
-    @recurrent(sequences=[], states=['states'], outputs=['states', 'flags'],
+    @recurrent(sequences=[], states=['states'],
+               outputs=['states', 'flags'],
                contexts=['inputs'])
     def apply(self, inputs=None, states=None, **kwargs):
         outputs = self.children[0].apply(
@@ -203,11 +143,6 @@ class DelayLine(BaseRecurrent, Initializable):
         add_role(self.mu, WEIGHT)
         if not self.fixed:
             self.params.append(self.mu)
-        '''
-        self.delay_line = shared_floatx_nans((self.batch_size,
-                                              self.input_dim*self.memory_size),
-                                              'delay_line')
-        '''
 
     def _initialize(self):
         mu, = self.params
