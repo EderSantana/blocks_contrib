f87ef3b9c9c4f022851b017c532233fd664d7be2
EderSantana <eder@macbook.com>
2015-05-15 14:53:13 -0400

    Added an approximate t-sne cost

--- bricks/cost.py
+++ bricks/cost.py
@@ -1,9 +1,13 @@
 import numpy as np
+import theano
 
 from theano import tensor
 
 from blocks.bricks.base import application
 from blocks.bricks.cost import Cost
+
+from blocks_contrib.utils import distance_matrix, zero_diagonal, l2
+floatX = theano.config.floatX
 
 
 class GaussianPrior(Cost):
@@ -22,3 +26,58 @@ class GaussianMSE(Cost):
         logpxz = ((0.5 * np.log(2 * np.pi) + log_sigma) +
                   0.5 * ((y - mean) / tensor.exp(log_sigma))**2).sum(axis=-1)
         return logpxz
+
+
+class tSNE(Cost):
+    @application(output=['cost'])
+    def apply(self, Y, X, perplexity):
+        '''t-SNE embedding cost
+
+        Parameters
+        ----------
+
+        X: `tensor.matrix`
+            Data points in the orignal space
+
+        Y: `tensor.matrix`
+            Embedding points in the mapping space
+
+        perplexity: float
+            Perplexity measure to control the value of kernel size in the
+            original space. Intuitevely, this is the number of neighbors
+            each data point should have.
+
+        alpha: float
+            Degrees of freedom in the Student-t distribution
+
+        References
+        ----------
+        .. [1] https://github.com/breze-no-salt/breze/blob/master/breze/learn/tsne.py
+        '''
+        q = self._get_probabilities_q(Y)
+        p = self._get_probabilities_p(X, perplexity)
+
+        # t-distributed stochastic neighbourhood embedding loss.
+        loss = (p * tensor.log(p / q)).sum(axis=-1)
+        return loss
+
+    def _get_probabilities_q(self, X):
+        dists = distance_matrix(X)
+        top = zero_diagonal(1 / (1 + dists))
+        bottom = top.sum(axis=0)
+        q = top / bottom
+        q /= q.sum()
+        q = tensor.maximum(q, 1e-12)
+        return q
+
+    def _get_probabilities_p(self, X, perplexity=30):
+        dists = distance_matrix(X, norm=lambda x, axis: l2(x, axis=axis)**2)
+        sigma = dists.sort(axis=1)[:, -perplexity] / 3
+        top = tensor.exp(-dists/(2*sigma[:, None]**2))
+        top = zero_diagonal(top)
+        bottom = top.sum(axis=0)
+        p = top / bottom
+        p = p + p.T
+        p /= p.sum()
+        p = tensor.maximum(p, 1e-12)
+        return p
--- utils.py
+++ utils.py
@@ -52,3 +52,80 @@ def _add_noise(n_steps, batch_size, dim, rfunc=np.random.normal):
         noise = rfunc(0, 1, size=(n_steps, batch_size, dim)).astype(floatX)
         return (noise,)
     return func
+
+
+def pairwise_diff(X, Y=None):
+    '''Given two arrays with samples in the row, compute the pairwise
+    differences.
+    Parameters
+    ----------
+
+    X : Theano variable
+        Has shape ``(n, d)``. Contains one item per first dimension.
+    Y : Theano variable, optional [default: None]
+        Has shape ``(m, d)``.  If not given, defaults to ``X``.
+
+    Returns
+    -------
+
+    res : Theano variable
+        Has shape ``(n, d, m)``.
+    '''
+    Y = X if Y is None else Y
+    diffs = X.T.dimshuffle(1, 0, 'x') - Y.T.dimshuffle('x', 0, 1)
+    return diffs
+
+
+def l2(X, axis=None):
+    return tensor.sqrt((X**2).sum(axis=axis) + 1e-8)
+
+
+def distance_matrix(X, Y=None, norm=l2):
+    '''Return an expression containing the distances given the norm of up to two
+    arrays containing samples.
+
+    Parameters
+    ----------
+
+    X : Theano variable
+        Has shape ``(n, d)``. Contains one item per first dimension.
+    Y : Theano variable, optional [default: None]
+        Has shape ``(m, d)``.  If not given, defaults to ``X``.
+    norm : string or callable
+        Either a string pointing at a function in ``breze.arch.component.norm``
+        or a function that has the same signature as these.
+    Returns
+    -------
+
+    res : Theano variable
+        Has shape ``(n, m)``.
+
+    References
+    ----------
+    .. [1] https://github.com/breze-no-salt/breze/blob/master/breze/learn/tsne.py
+    '''
+    diff = pairwise_diff(X, Y)
+    dist = norm(diff, axis=1)
+    return dist
+
+
+def zero_diagonal(X):
+    '''Given a square matrix ``X``, return a theano variable with the diagonal
+    of ``X`` set to zero.
+
+    Parameters
+    ----------
+
+    X : theano 2d tensor
+
+    Returns
+    -------
+
+    Y : theano 2d tensor
+
+    References
+    ----------
+    .. [1] https://github.com/breze-no-salt/breze/blob/master/breze/learn/tsne.py
+    '''
+    thisid = tensor.identity_like(X)
+    return (X - thisid * X)
