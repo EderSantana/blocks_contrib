4bbcfc079c006986afc7e046f2839bf62f24ea75
EderSantana <eder@macbook.com>
2015-05-26 20:27:51 -0400

    Added helper functions to utils
    These helper functions should go to their own file. To specific to be on
    the general utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
 import numpy as np
 import theano
+import h5py
 
+from scipy.spatial.distance import squareform
 from sklearn.metrics.pairwise import pairwise_distances
+from sklearn.manifold.t_sne import _joint_probabilities
 from theano import tensor
 from blocks.filter import VariableFilter, get_brick
 from blocks.roles import OUTPUT, PARAMETER, add_role
@@ -207,3 +210,32 @@ def neighbour_probabilities(X, target_pplx):
         p_ji = np.maximum(1E-12, p_ji)
 
         return p_ji
+
+
+def get_probability_matrices(datastream, num_batches, h5path, perplexity=30):
+    '''Get joint probability matrices
+
+    Uses sklearn _joint_probabilities function to all calculate the probability
+    matrices of all the batches of a `fuel.datastream`. This function assumes that
+    the data of interest is the first elemeent of each batch. Also it assumes
+    that the data was already transposed to the form time x batch x dimension.
+    '''
+    h5obj = h5py.File(h5path)
+    dt = h5py.special_dtype(vlen=np.dtype(floatX))
+    d = h5obj.create_dataset('probability_matrices', (num_batches, ), dtype=dt)
+    for i, b in enumerate(datastream.get_epoch_iterator()):
+        D = pairwise_distances(b[0], metric='euclidean', squared=True)
+        P = _joint_probabilities(D.astype('float64'), perplexity, verbose=False)
+        d[i] = P
+    h5obj.close()
+
+
+def probability_matrices_generator(h5path, num_batches):
+    i = -1
+    h5obj = h5py.File(h5path, 'r')
+    P = h5obj['probability_matrices']
+    while True:
+        i += 1
+        idx = i % num_batches
+        yield squareform(P[idx])
+    h5obj.close()
